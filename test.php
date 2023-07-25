31bea1e4a0ea504f8e2438dd8dc9633c3d2dcb3c
ricardobarbosams <53918764+ricardobarbosams@users.noreply.github.com>
2023-07-24 22:44:33 -0400

    Update test.php

--- test.php
+++ test.php
@@ -28,22 +28,6 @@ echo "<h1>Test app</h1>";
 echo "<h1>Test app</h1>";
 echo "<h1>Test app</h1>";
 
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
 
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
-echo "<h1>Test app</h1>";
 
 ?>
